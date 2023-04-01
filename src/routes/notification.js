0971b55fb5d3edec38b4ef21c7f0a9c8584b4d87
PracticalDev <carlosospinomonsalve@gmail.com>
2023-04-01 11:12:11 -0500

    Secured route notification.js

--- src/routes/notification.js
+++ src/routes/notification.js
@@ -1,8 +1,10 @@
 const notification_controller = require("../controllers/notification");
+const passport = require("passport");
+require("../security/passport")
 
 const routes = app => {
 
-    app.get("/api/notification", notification_controller.notify);
+    app.get("/api/notification", passport.authenticate('jwt', { session: false }),notification_controller.notify);
 
 }
 
