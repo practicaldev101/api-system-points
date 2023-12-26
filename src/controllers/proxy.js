c1601daa823d2bb4ec5a2b848dcaf4f9d5ba18fa
Carlos Ospino <88525633+practicaldev101@users.noreply.github.com>
2023-12-26 16:09:23 -0500

    Recognize query params

--- src/controllers/proxy.js
+++ src/controllers/proxy.js
@@ -3,8 +3,8 @@ const fetch = require('node-fetch-commonjs');
 const controller = {}
 
 controller.getServersByPlaceId = async(req, res) => {
-    const {placeId, sortOrder, limit} = req.params;
-    const request = await fetch(`https://games.roblox.com/v1/games/${placeId || 13389122030}/servers/Public?sortOrder=${sortOrder|| "Asc"}&limit=${limit || 100}`, {method: "GET"})
+    const {placeId, sortOrder, limit} = req.query;
+    const request = await fetch(`https://games.roblox.com/v1/games/${placeId || 13550916422}/servers/Public?sortOrder=${sortOrder|| "Asc"}&limit=${limit || 100}`, {method: "GET"})
     const servers = await request.json()
     res.status(200).json(servers)
 }
